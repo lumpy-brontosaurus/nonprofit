21c70798198093919aa09093d4823f01492a75f5
hridhya <menonhridhya@gmail.com>
2016-01-07 10:08:08 -0800

    map added

--- client/app/app.js
+++ client/app/app.js
@@ -1,23 +1,30 @@
-var app = angular.module('volunteer', ['ui.router']);
-
+var app = angular.module('volunteer', ['ui.router', 'ngMap']);
+//
 app.controller('MainCtrl', ['$scope', function($scope){
 
     $scope.searchResult = [];
     $scope.filterContents = function(){
+        $scope.searchResult.push({interest:$scope.item,zip:$scope.zip})
+    };
 
-    }
+}]);
 
 
-    }]);
+app.config(function($stateProvider, $urlRouterProvider) {
 
-app.config(function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/home');
 
-    $urlRouterProvider
-        .otherwise('/');
     $stateProvider
+
         .state('home', {
-            url: '/',
-            templateUrl: 'app/volunteer/home.html',
-            controller: 'MainCtrl'
+            url: '/home',
+            templateUrl: 'app/volunteer/home.html'
+        })
+
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'app/volunteer/map.html'
+
         });
+
 });
